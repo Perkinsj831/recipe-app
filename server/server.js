0607b0e94147cedd48b12f1378a335bdc2da32f6
John Perkins <mrgreen@MrGreens-Laptop.local>
2024-06-11 16:21:20 -0500

    Initial commit for deployment

--- server/server.js
+++ server/server.js
@@ -26,7 +26,10 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => {
     console.log('MongoDB connected');
   })
